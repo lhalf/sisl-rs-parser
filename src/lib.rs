09adf55ef9710cd8f9bdc5478139b0832ad34eae
Laurence <laurence.halfpenny@halfpenny.net>
2022-05-01 06:28:18 -0700

    add initial integer parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub integers);
+
+#[test]
+fn integers() {
+    assert!(integers::TermParser::new().parse("22").is_ok());
+    assert!(integers::TermParser::new().parse("(22)").is_ok());
+    assert!(integers::TermParser::new().parse("((((22))))").is_ok());
+    assert!(integers::TermParser::new().parse("((22)").is_err());
 }
