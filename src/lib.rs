02783bd3a8692e32f816d6e6feb12a6d62767db9
Laurence <laurence.halfpenny@halfpenny.net>
2022-05-05 11:00:17 -0700

    update grammar to support { and "

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ lalrpop_mod!(pub integers);
 #[test]
 fn integers() {
     assert!(integers::TermParser::new().parse("22").is_ok());
-    assert!(integers::TermParser::new().parse("{22}").is_ok());
-    assert!(integers::TermParser::new().parse("{{{{22}}}}").is_ok());
-    assert!(integers::TermParser::new().parse("{{22}").is_err());
+    assert!(integers::TermParser::new().parse("{\"22\"}").is_ok());
+    assert!(integers::TermParser::new().parse("{22\"}").is_err());
 }
