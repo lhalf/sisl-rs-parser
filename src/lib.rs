83fab60bb5102bebd36b2bc949bd2d2dcf511d0c
Laurence <laurence.halfpenny@halfpenny.net>
2022-05-23 12:30:55 -0700

    update i32 parser

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,20 @@
 lalrpop_mod!(pub integers);
 
 #[test]
-fn integers() {
+fn i32() {
     assert!(integers::TermParser::new().parse("22").is_ok());
-    assert!(integers::TermParser::new().parse("{\"!i32 22\"}").is_ok());
-    assert!(integers::TermParser::new().parse("\"!i32 22\"}").is_err());
-    assert!(integers::TermParser::new().parse("{\"i32 22\"}").is_err());
-    assert!(integers::TermParser::new().parse("{!i32 22\"}").is_err());
-    assert!(integers::TermParser::new().parse("{\"!i32 22}").is_err());
-    assert!(integers::TermParser::new().parse("{\"!i32 22\"").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !\"_i32\" \"22\"}").is_ok());
+    assert!(integers::TermParser::new().parse("\"_\": !\"_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{_\": !\"_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"\": !\"_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_: !\"_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\" !\"_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": \"_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !_i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !\"i32\" \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !\"_i32 \"22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !\"_i32\" 22\"}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !\"_i32\" \"22}").is_err());
+    assert!(integers::TermParser::new().parse("{\"_\": !\"_i32\" \"22\"").is_err());
 }
